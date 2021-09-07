504014e03df063d4fb3ef757024b22d3d36ba824
1234567890num <53823593+1234567890num@users.noreply.github.com>
2021-09-08 04:38:02 +0800

    Initial Upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Music-Randomizer-PC
+A mod for the OpenKH Mod Manager that randomizes the KH2 Music along with any other additions.
+
+## How to use:
+* Navigate to the extracted KH2PC folder
+* Copy the bgm and vagstream folders
+* Install the mod using OpenKH Mod Manager
+* Navigate to the OpenKH `Mods` folder
+* Paste the folders copied earlier
+* Make a new `bgmnew` folder
+* Copy all the desired custom music into that folder.
+* Feel free to have subfolders within that folder or to rename the music files, but only the files that end in `.scd` will be considered.
+* Run `Randomize.exe`
+* Follow the steps as if you just installed a zip seed to apply it to your PC.
+* Your music is now randomized!
---
+++ Randomize.py
@@ -0,0 +1,31 @@
+import os
+import random
+
+
+
+#Get KH2 music filenames
+currentDir = os.path.realpath(__file__).replace(os.path.basename(__file__),'')
+musiclist = []
+for folder in ['bgm','vagstream']:
+    files = os.listdir(currentDir+folder)
+    for file in files:
+        if file[-4:] == '.scd':
+            musiclist.append('bgm\\'+file)
+
+#Get new music filenames
+randomlist = musiclist.copy()
+dirstart = len(currentDir)
+for root, dirs, files in os.walk(currentDir+'bgmnew'):
+    for file in files:
+        if file[-4:] == '.scd':
+            randomlist.append(root[dirstart:]+'\\'+file)
+random.shuffle(randomlist)
+
+#Write the mod.yml
+f = open(currentDir+'mod.yml','w')
+f.write('assets:\n')
+for i in range(len(musiclist)):
+    old = musiclist[i]
+    new = randomlist[i]
+    f.write('- name: '+old+'\n  method: copy\n  source:\n  - name: '+new+'\n')
+f.close()
